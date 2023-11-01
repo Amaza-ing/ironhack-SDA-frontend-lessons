95c3b569ee5a449c719badcb6e362bcbcafe6a8c
Adrián Maza <adri_mv1@hotmail.com>
2023-11-01 08:36:38 +0100

    basics created

---
+++ basics.js
@@ -0,0 +1,16 @@
+console.log("Hello everyone!");
+
+let myVar = 10;
+console.log("myVar: ", myVar, typeof myVar);
+
+let myStr = "Hello!";
+console.log("myStr:", myStr, typeof myStr);
+console.log(myStr.length, myStr[0], myStr.charAt(0));
+
+let name = 'Adrián';
+console.log("name:", name);
+
+let info = `this is some info: ${name}`;
+console.log(info);
+
+const myNum = 50;
